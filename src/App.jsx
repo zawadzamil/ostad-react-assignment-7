730469833526bcdf1313774628dfefdd6526ed4e
zawadzamil <contact.zawadzamil@gmail.com>
2023-07-17 19:22:32 +0600

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from "react";
+
+import Header from "./assets/components/Header";
+import Input from "./assets/components/Input";
+
+function App() {
+    // const [count, setCount] = useState(0);
+
+    return (
+        <>
+            <Header></Header>
+            <Input></Input>
+        </>
+    );
+}
+
+export default App;
---
+++ src/assets/components/Header.jsx
@@ -0,0 +1,13 @@
+// import React from "react";
+
+export default function Header() {
+    return (
+        <>
+            <div className="container-fluid bg-dark mb-4">
+                <h3 className="text-light text-center p-4 display-4">
+                    Todo Application
+                </h3>
+            </div>
+        </>
+    );
+}
